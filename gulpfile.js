1ce018339b9b86b896f4e11b9cdb8a768f82b5c0
Pawel Barszcz <pawelbarszcz@gmail.com>
2015-05-27 22:30:40 +0200

    Configured 'serve' task for Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp');
+var webserver = require('gulp-webserver');
 
 gulp.task('default', function () {
   // place code for your default task here
+});
+
+gulp.task('serve', function () {
+  gulp.src('src')
+    .pipe(webserver({
+      livereload: true,
+      open: 'index.html'
+    }));
 });
