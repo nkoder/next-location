758c7d494dea443ed30b9dd0bd505aaba63b92ae
Pawel Barszcz <pawelbarszcz@gmail.com>
2015-05-30 17:17:07 +0200

    Polish specific characters are replaced by simplified ones in sanitizer when matching answers

--- src/js/sanitizer.js
+++ src/js/sanitizer.js
@@ -1,7 +1,20 @@
 define(['lodash'], function (_) {
 
   function sanitizeText(text) {
-    return _.deburr(_.escape(text)).toLowerCase();
+    return simplifyPolishCharacters(_.deburr(_.escape(text)).toLowerCase());
+  }
+
+  function simplifyPolishCharacters(text) {
+    return text
+      .replace('ą', 'a')
+      .replace('ć', 'c')
+      .replace('ę', 'e')
+      .replace('ł', 'l')
+      .replace('ń', 'n')
+      .replace('ó', 'o')
+      .replace('ś', 's')
+      .replace('ź', 'z')
+      .replace('ż', 'z');
   }
 
   return {
